ca07fbdfc187aac3d82ff74347e87576886ef195
안철민 <81482706+cjfals9626@users.noreply.github.com>
2024-03-01 17:26:52 +0900

    하트비트 Scheduling 어노테이션 적용

--- src/main/java/org/spring/pizzarazzi_alarm/PizzarazziAlarmApplication.java
+++ src/main/java/org/spring/pizzarazzi_alarm/PizzarazziAlarmApplication.java
@@ -2,8 +2,10 @@ package org.spring.pizzarazzi_alarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PizzarazziAlarmApplication {
 
     public static void main(String[] args) {
